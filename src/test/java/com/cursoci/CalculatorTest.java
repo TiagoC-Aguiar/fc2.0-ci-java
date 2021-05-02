fcbdee1adf740d2a420af284edb27efd3a8bc267
Tiago Aguiar <tiago.aguiar@quickks.com>
2021-05-02 10:50:19 -0300

    feat: :sparkles: adicionado função de soma

---
+++ src/test/java/com/cursoci/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.cursoci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void deveSomarDoisNumeros() {
+        Calculator calc = new Calculator();
+        int soma = calc.soma(10, 20);
+
+        assertEquals(30, soma);
+    }
+}
