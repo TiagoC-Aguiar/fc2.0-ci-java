1018f82cb16b1dc03a99dc2bb0424f5ce4e7acdd
Tiago Aguiar <tiago.aguiar@quickks.com>
2021-05-02 11:25:17 -0300

    ci: :green_heart: add github actions for every push

---
+++ src/main/java/com/cursoci/CursoCI.java
@@ -0,0 +1,9 @@
+package com.cursoci;
+
+public class CursoCI {
+    public static void main(String[] args) {
+
+        System.out.println(new Calculator().soma(10, 11));
+
+    }
+}
--- src/test/java/com/cursoci/CalculatorTest.java
+++ src/test/java/com/cursoci/CalculatorTest.java
@@ -1,9 +1,8 @@
 package com.cursoci;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
